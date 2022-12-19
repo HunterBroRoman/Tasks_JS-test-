67f6ddc6a3cfd5ed1b46aa0fbb9ed88babca8996
Roman <sayga76239@gmail.com>
2022-12-19 19:47:44 +0100

    11

--- js/poliandr_test.js
+++ js/poliandr_test.js
@@ -1,9 +1,4 @@
-function palindrome(str) {
-    let check = '';
-    for (let i = str.length - 1; i >= 0; --i) {
-      check += str[i];
-    }
-    return str == check;
-  }
-  console.log(palindrome('abcdcba'));
-//перебор начинается с последней буквы строки (length-1),которые формируют новую перевернутую строку 
+function palindrome(x) {
+   return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
+}
+console.log(palindrome('madam')); 
